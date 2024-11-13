543b0bd5ebe0cffc7408be19ca89f864a791edd9
jisu_oh <jisu_oh@tmax.co.kr>
2024-11-14 00:10:20 +0900

    [feat] cors 설정 완료

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-app.get("/", () => {
-  console.log("?");
-});
+const handleHome = (req, res) => {
+  return res.send("Welcome home");
+};
+
+app.use(cors({ origin: "http://localhost:5173" }), logger);
+app.get("/", handleHome);
 
 const handleListening = () => {
   console.log(`Server listening on port ${PORT}`);
