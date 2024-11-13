5cc1e56b229dedd2c3878e93b7a3b3b5e6e3c355
jisu_oh <jisu_oh@tmax.co.kr>
2024-11-14 00:24:49 +0900

    [feat] 기본 컨트롤러 생성

---
+++ server/controllers/userController.js
@@ -0,0 +1,4 @@
+export const join = (req, res) => res.send("Join");
+export const login = (req, res) => res.send("Login");
+export const editUser = (req, res) => res.send("Edit User");
+export const deleteUser = (req, res) => res.send("Delete User");
---
+++ server/controllers/videoController.js
@@ -0,0 +1,4 @@
+export const homeVideos = (req, res) => res.send("Home Videos");
+export const watch = (req, res) => res.send("Watch");
+export const editVideo = (req, res) => res.send("Edit Video");
+export const deleteVideo = (req, res) => res.send("Delete Video");
