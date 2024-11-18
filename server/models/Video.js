a6ff1e02326ef7edb2c8d71e0aa9913b7aba1521
jisu_oh <jisu_oh@tmax.co.kr>
2024-11-18 23:27:47 +0900

    [feat] video model 생성

---
+++ server/models/Video.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+// 비디오 형태 정하는 부분
+const videoSchema = new mongoose.Schema({
+  title: { type: String, required: trie, trim: true, maxLength: 80 },
+  fileUrl: { type: String, required: true },
+  thumbUrl: { type: String, required: true },
+  description: { type: String, required: true, trim: true, minLength: 2 },
+  createdAt: { type: Date, required: true, default: Date.now },
+  hashtags: [{ type: String, trim: true }],
+  meta: {
+    views: { type: Number, default: 0, required: true },
+  },
+  comment: [
+    { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Comment" },
+  ],
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+});
+
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
+    .split(",")
+    .map((tag) => (tag.startsWith("#") ? tag : `#${tag}`));
+});
+
+const Video = mongoose.model("Video", videoSchema);
+
+export default Video;
